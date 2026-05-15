59b0ef22f78b456a5ec990700dcc8fdfc2be3772
DryBreadThief <theodoros.niarchos.p@gmail.com>
2026-05-15 17:47:20 +0000

    Initial

---
+++ src/main/java/com/niarchos/portfolio/portofolio_back_end/PortofolioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.niarchos.portfolio.portofolio_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortofolioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortofolioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niarchos/portfolio/portofolio_back_end/PortofolioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niarchos.portfolio.portofolio_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortofolioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
